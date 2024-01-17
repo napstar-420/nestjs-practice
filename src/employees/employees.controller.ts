cdf61b3f884ee7bfacea4db754dbdcd38637e55b
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-01-17 18:17:30 +0500

    create Employees REST API

---
+++ src/employees/employees.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  ParseIntPipe,
+  ValidationPipe,
+} from '@nestjs/common';
+import { EmployeesService } from './employees.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('employees')
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post()
+  create(@Body(ValidationPipe) createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.employeesService.create(createEmployeeDto);
+  }
+
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.employeesService.findAll(role);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.employeesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
+  ) {
+    return this.employeesService.update(+id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeesService.remove(+id);
+  }
+}
